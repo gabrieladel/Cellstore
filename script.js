5af67fe6fc2b47230ee520520f4d5f117cd7b267
gabrieladel <gabi23@live.com.ar>
2023-04-24 12:25:59 -0300

    cambios en css

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 //elemento donde incoorporo el texto
 const texto = document.getElementById("Nosotros");
 const div = document.createElement("div");
-div.setAttribute("class", "row bg-light text-body-secondary"); //agrego al div la clase
+div.setAttribute("class", "row text-body-light"); //agrego al div la clase
+
 texto.appendChild(div);//se posiciona dentro del texto(Nosotros)
+const h2 = document.createElement("h2")
+h2.textContent = "Nuestra historia"
 //creo el tag legend
 const legend = document.createElement("legend");
 //agrego texto al legend
 legend.textContent = "Somos una empresa que se orem ipsum dolor, sit amet consectetur adipisicing elit. Iusto veniam id alias earum non, quaerat nobis sapiente officiis fugiat hic iure perspiciatis, nihil maxime explicabo quis impedit modi. Earum, saepe.";
-
+div.appendChild(h2);
 //incoorporo al div del nosotros
 div.appendChild(legend);
